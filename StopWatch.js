acb5e15c9dc14dd3b227e0e18a6b627710018a9a
Von'Erik Barren-Tucker <barrentucker.vonerik@gmail.com>
2020-08-19 08:18:28 -0400

    started function phase --> initialization of variables

--- StopWatch.js
+++ StopWatch.js
@@ -1,1 +1,16 @@
-console.log("Testing 123")
+window.onload = function () {
+
+  //--> Initialization of Variables 
+  let seconds = 00;
+  let tens = 00;
+  let appendTens = document.getElementsByClassName("tens");
+  let appendSeconds = document.getElementsByClassName("seconds");
+  let buttonStart = document.getElementsByClassName("button-start");
+  let buttonStop = document.getElementsByClassName("button-stop");
+  let buttonReset = document.getElementsByClassName("button-reset");
+  let Interval;
+
+
+
+  //--> final bracket
+}
